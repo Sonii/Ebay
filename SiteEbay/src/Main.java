22215370f5d4d32c14e42eb16b0337aadad8a7fc
hajarmalil <malil@et.esiea.fr>
2014-03-16 06:10:23 +0100

    Changement Architectre

--- SiteEbay/src/Main.java
+++ SiteEbay/src/Main.java
@@ -8,11 +8,16 @@ public class Main {
 		UsineUtilisateur usine = new UsineUtilisateur();
 		Utilisateur toto = usine.CreeUtilisateur(TypeUtilisateur.Vendeur, "toto", "", "", alerte);
 		System.out.println(alerte.utilisateur.get(0).login); 
-		Enchere en = toto.creeEnchere(110, 0, "", "", "");
-		System.out.println(toto.encheres.get(0).prixMinimum);
+		//Enchere en = toto.creeEnchere(110, 0, "", "", "");
+		//System.out.println(toto.encheres.get(0).prixMinimum);
 		Utilisateur hajar = usine.CreeUtilisateur(TypeUtilisateur.Achteur, "hajar", "", "", alerte);
-		Enchere e = hajar.creeEnchere(10, 0, "", "", "");
-		Offre o = hajar.creeOffre(e, 0);
+		//Enchere e = hajar.creeEnchere(10, 0, "", "", "");
+		//Offre o = hajar.creeOffre(e, 0);
+		Enchere enn = toto.creeEnchere(0, 0, "", "", "");
+		Offre off = toto.creeOffre(enn, 10);
+		//System.out.println(toto.offres.get(0).getPrixOffre());
+		Offre offf = hajar.creeOffre(enn, 100);
+		System.out.println(hajar.offres.get(0).prixOffre);
 		
 		/*Vendeur a = new Vendeur("toto", "", "");
 		Enchere en = a.creationEnchere(0, 0, "", "", "");
--- SiteEbay/src/TypeUtilisateur.java
+++
@@ -1,5 +0,0 @@
-
-public enum TypeUtilisateur {
-	Vendeur,
-	Achteur
-}
